a7ba924cc2f2d1381d90b260ad2cdbc69c11fedc
sheng wei qiu <shadow3x3x3@gmail.com>
2017-06-25 22:30:11 +0800

    Weather API get weather now and aqi

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
 /* eslint-disable no-new */
+Vue.prototype.$http = axios
+
+const SERVER_ADDRESS = 'http://140.134.26.64:1234/wmega/webapi/'
+
 var weather = new Vue({
   el: '#weather',
   data: {
+    WEATHER_NOW_ADDRESS: SERVER_ADDRESS + 'weather/now/',
+    AQI_NOW_ADDRESS: SERVER_ADDRESS + 'air/aqi/',
+    localCoords: '24.178863/120.646648',
+    wjson: '',
+    weatherNow: {
+      'h_24r': 0,
+      'humd': 0,
+      'temp': 0,
+      'wdir': '',
+      'wdsd': 0
+    },
+    aqiNow: {
+      'aqi': 0,
+      'co': 0,
+      'no': 0,
+      'no2': 0,
+      'nox': 0,
+      'o3': 0,
+      'pm10': 0,
+      'pm25': 0,
+      'so2': 0
+    }
   },
   methods: {
     dateToday: function () {
@@ -16,5 +42,25 @@ var weather = new Vue({
              dateToday.getDate() + '日' +
              '(' + weekdayText[dateToday.getDay()] + ')'
     }
+  },
+  created () {
+    this.$http.all([
+      this.$http.get(this.WEATHER_NOW_ADDRESS + this.localCoords),
+      this.$http.get(this.AQI_NOW_ADDRESS + this.localCoords)
+    ])
+      .then(this.$http.spread(function (weatherNowJson, aqiNowJson) {
+        // Weather JSON Process
+        for (let key in weather.$data.weatherNow) {
+          weather.$data.weatherNow[key] = weatherNowJson.data[key]
+        }
+
+        // AQI JSON Processs
+        for (let key in weather.$data.aqiNow) {
+          weather.$data.aqiNow[key] = aqiNowJson.data['values'][key]
+        }
+      }))
+      .catch(function (error) {
+        console.log(error)
+      })
   }
 })
